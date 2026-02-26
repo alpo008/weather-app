bdd4a491222131d4e39497a515302c460bc301b8
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2026-02-26 13:11:58 +0300

    Style for light theme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="main-section">
+  <main class="main-section theme-light">
     <div class="weather" :style="toggler_style">
       <h2 @click="start">{{ _t('Our meteostation') }}</h2>
       <div v-if="show"> 
